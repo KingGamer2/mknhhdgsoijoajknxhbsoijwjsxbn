759fe6884581a714acf39d21d9e9f76963f762ea
KingGamer2 <40805903+KingGamer2@users.noreply.github.com>
2018-08-14 23:34:01 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -730,5 +730,29 @@ hours = 12;
     });
   }
 });
+const jackeo = ['346067998794317824' , '461583618238578688' , '352832614190809089' , '']; //Jackeo  ?????
+client.on('message', message => { 
+var prefix = "%"; 
+  if (message.author.bot) return; 
+  if (!message.content.startsWith(prefix)) return;
+    var argresult = message.content.split(` `).slice(1).join(' '); 
+      if (!jackeo.includes(message.author.id)) return; 
+  let command = message.content.split(" ")[0]; 
+  command = command.slice(prefix.length);
+  let args = message.content.split(" ").slice(1);
+  if (command === "say")  { 
+  if(!message.channel.guild) return message.reply('** __This command only for servers?__  **'); 
+          message.delete() 
+    message.channel.sendMessage(args.join(" ")).catch(console.error); 
+  }
+if (command == "emb")    { 
+  if(!message.channel.guild) return message.reply('** __This command only for servers?__  **'); 
+    let say = new Discord.RichEmbed() 
+    .setDescription(args.join("  ")) 
+    .setColor("RANDOM") 
+    message.channel.sendEmbed(say); 
+    message.delete(); 
+  } 
+});
 
 client.login(process.env.BOT_TOKEN);
