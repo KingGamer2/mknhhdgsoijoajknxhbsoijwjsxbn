6d75bdd7873dab2f44ce0df2c5db3bb5bd8f2920
KingGamer2 <40805903+KingGamer2@users.noreply.github.com>
2018-08-14 05:45:37 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -565,20 +565,5 @@ if (message.content.startsWith(prefix+"voice")) {
          
         }
 });
-const child_process = require("child_process");
-const adminprefix = "-";
-const devs = ['352832614190809089'];
-
-client.on('message', message => {
-if(message.content === adminprefix + "restart") {
-      if (!devs.includes(message.author.id)) return;
-          message.channel.send(⚠️ **الشخص الذي اعاد تشغيل البوت ${message.author.username}**);
-        console.log(⚠️ جاري اعادة تشغيل البوت... ⚠️);
-        client.destroy();
-        child_process.fork(__dirname + "/restart.js");
-        console.log(تم اعادة تشغيل البوت);
-    }
-  
-  });
 
 client.login(process.env.BOT_TOKEN);
